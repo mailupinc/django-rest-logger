9af2b8080ac64213715ce23cb07ef77212d182e9
Roberto Pomoni <roberto.pomoni@beefree.io>
2021-06-16 12:03:30 +0200

    Use only pytest for testing

--- restlogger/tests/test_middleware.py
+++ restlogger/tests/test_middleware.py
@@ -1,33 +1,26 @@
-from unittest import mock
-
-from django.http import HttpResponse, HttpResponseServerError
-from django.test import RequestFactory, SimpleTestCase
-
-import restlogger
-from restlogger.middleware import RESTRequestLoggingMiddleware
-
-
-def get_empty_response(request):
-    return HttpResponse()
-
-
-def get_response_server_error(request):
-    return HttpResponseServerError()
+def test_base_logging_with_standard_request(
+    API_request_factory, middleware_empty_django_response, mocked_logger
+):
+    request = API_request_factory.get("/foo")
+    middleware_empty_django_response(request)
+    name, args, kwargs = mocked_logger.mock_calls[0]
+    assert name == "info"
+    assert kwargs["extra"]
+    assert kwargs["extra"]["request"]["path"] == "/foo"
+    assert kwargs["extra"]["request"]["method"] == "GET"
+    assert kwargs["extra"]["response"]
+    assert kwargs["extra"]["response"]["status_code"] == 200
 
 
-class RESTRequestLoggingMiddlewareTest(SimpleTestCase):
-    def setUp(self):
-        self.factory = RequestFactory()
-        self.middleware = RESTRequestLoggingMiddleware(get_empty_response)
-
-    @mock.patch.object(restlogger.middleware, "log")
-    def test_base_logging(self, mock_logger):
-        request = self.factory.get("/foo")
-        self.middleware(request)
-        name, args, kwargs = mock_logger.mock_calls[0]
-        assert name == "info"
-        assert kwargs["extra"]
-        assert kwargs["extra"]["request"]["path"] == "/foo"
-        assert kwargs["extra"]["request"]["method"] == "GET"
-        assert kwargs["extra"]["response"]
-        assert kwargs["extra"]["response"]["status_code"] == 200
+def test_base_logging_with_api_request(
+    API_request_factory, middleware_empty_api_response, mocked_logger
+):
+    request = API_request_factory.get("/foo", format="json")
+    middleware_empty_api_response(request)
+    name, args, kwargs = mocked_logger.mock_calls[0]
+    assert name == "info"
+    assert kwargs["extra"]
+    assert kwargs["extra"]["request"]["path"] == "/foo"
+    assert kwargs["extra"]["request"]["method"] == "GET"
+    assert kwargs["extra"]["response"]
+    assert kwargs["extra"]["response"]["status_code"] == 200
